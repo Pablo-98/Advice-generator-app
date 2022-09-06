146465f5019197e8b06cab3b790a7f0f8baf7352
Pablo Hernandez <hpablo575@gmail.com>
2022-09-05 21:31:28 -0600

    added fetch request in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-var apiUrl = 'https://api.adviceslip.com'
+//this is my url
+//var apiUrl = 'https://api.adviceslip.com'
+
+//this is one method I found online about how to do a GET request but it seems weird. A fetch may be more appropriate
+//function httpGet('https://api.adviceslip.com/advice') {
+   // let xmlHttpReq = new XMLHttpRequest();
+    //xmlHttpReq.open("GET",'https://api.adviceslip.com/advice', true );
+    //xmlHttpReq.send(null);
+   // return xmlHttpReq.responseText;
+// }
+
+
+// this successfully took the advice and put it in the console.
+fetch ('https://api.adviceslip.com/advice')
+.then(res => res.json())
+.then(data => console.log(data))
+
+
+ 
