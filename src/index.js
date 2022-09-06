5511335617990549cffbda7f390840b9d87714ea
Pablo Hernandez <hpablo575@gmail.com>
2022-09-06 11:37:54 -0600

    styled box and background, added button and divider, linked the fetch request with the DOM

--- src/index.js
+++ src/index.js
@@ -8,12 +8,32 @@
     //xmlHttpReq.send(null);
    // return xmlHttpReq.responseText;
 // }
+const resDiv = document.querySelector('#results');
+const resBtn = document.querySelector('#get-advice');
 
+resBtn.addEventListener('click', () => {
+   getAdvice();
+});
+
+window.onload = () =>{
+   getAdvice();
+}
+
+function getAdvice(){
+
+   fetch ('https://api.adviceslip.com/advice')
+.then(res => {
+   return res.json();
+}).then(adviceData => {
+   const Adviceobj = adviceData.slip ;
+   resDiv.innerHTML = `<p> ${Adviceobj.advice} </p>`;
+}).catch(error => {
+   console.log(error);
+});
+
+}
 
 // this successfully took the advice and put it in the console.
-fetch ('https://api.adviceslip.com/advice')
-.then(res => res.json())
-.then(data => console.log(data))
 
 
  
